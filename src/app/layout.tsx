a73ae6c06e708db6ce6b4dd5a4f6445c3edcfbea
ImFeH2 <i@feh2.im>
2026-04-26 16:45:08 +0800

    chore: scaffold next app

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
+
+export const metadata: Metadata = {
+  title: "Flowent",
+  description: "Flowent application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("font-sans", geist.variable)}>
+      <body>{children}</body>
+    </html>
+  );
+}
